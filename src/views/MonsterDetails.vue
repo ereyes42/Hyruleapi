be3760cda6539307323521f8eff217c196a97dfe
Ember Reyes <ereyes42@stu.southtexascollege.edu>
2022-10-08 10:45:14 -0500

    changed syling on details page

--- src/views/MonsterDetails.vue
+++ src/views/MonsterDetails.vue
@@ -18,8 +18,8 @@ onUnmounted(() => {
 <template>
   <main class="min-h-screen bg-gradient-to-r from-green-400 to-green-900 py-8 text-white">
     <div v-if="currentMonster" class="flex flex-col items-center justify-center gap-6">
-      <img :src="currentMonster.image" :alt="currentMonster.name" />
-      <h1 class="text-white-800 text-6xl font-semibold">Hi, I'm {{ currentMonster.name }}</h1>
+      <img class="rounded-lg" :src="currentMonster.image" :alt="currentMonster.name" />
+      <h1 class="text-6xl font-semibold text-yellow-100">Hi, I'm {{ currentMonster.name }}</h1>
       <pre>{{ currentMonster.common_locations }}</pre>
     </div>
   </main>
